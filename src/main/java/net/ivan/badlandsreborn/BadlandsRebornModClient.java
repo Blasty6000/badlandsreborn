9d22debd711be771478f252ed2c057903967d678
Blasty6000 <ivanasen77@gmail.com>
2024-11-25 21:43:55 +0200

    Initial commit

---
+++ src/main/java/net/ivan/badlandsreborn/BadlandsRebornModClient.java
@@ -0,0 +1,10 @@
+package net.ivan.badlandsreborn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BadlandsRebornModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
